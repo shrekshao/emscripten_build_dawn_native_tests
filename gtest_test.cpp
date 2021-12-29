559307b46a7ea1913cef0223ff8f013ff5057b41
Shrek Shao <shrekshao@google.com>
2021-12-29 11:17:12 -0800

    wip changes for basic validation test run (dawn hack change not included

--- gtest_test.cpp
+++ gtest_test.cpp
@@ -1,3 +1,18 @@
+
+// #ifdef __EMSCRIPTEN__
+#include <webgpu/webgpu_cpp.h>
+
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#include <emscripten/html5_webgpu.h>
+// #endif  // __EMSCRIPTEN__
+
+#undef NDEBUG
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+
 // #include <gtest/gtest.h>
 // // #include "out/web/_deps/googletest-src/googletest/include/gtest/gtest.h"  // temp
 
@@ -35,19 +50,19 @@
 
 // class BasicTests : public DawnTest {};
 
-// // Test adapter filter by vendor id.
-// TEST_P(BasicTests, VendorIdFilter) {
-//     DAWN_TEST_UNSUPPORTED_IF(!HasVendorIdFilter());
+// // // Test adapter filter by vendor id.
+// // TEST_P(BasicTests, VendorIdFilter) {
+// //     DAWN_TEST_UNSUPPORTED_IF(!HasVendorIdFilter());
 
-//     ASSERT_EQ(GetAdapterProperties().vendorID, GetVendorIdFilter());
-// }
+// //     ASSERT_EQ(GetAdapterProperties().vendorID, GetVendorIdFilter());
+// // }
 
-// // Test adapter filter by backend type.
-// TEST_P(BasicTests, BackendType) {
-//     DAWN_TEST_UNSUPPORTED_IF(!HasBackendTypeFilter());
+// // // Test adapter filter by backend type.
+// // TEST_P(BasicTests, BackendType) {
+// //     DAWN_TEST_UNSUPPORTED_IF(!HasBackendTypeFilter());
 
-//     ASSERT_EQ(GetAdapterProperties().backendType, GetBackendTypeFilter());
-// }
+// //     ASSERT_EQ(GetAdapterProperties().backendType, GetBackendTypeFilter());
+// // }
 
 // // Test Queue::WriteBuffer changes the content of the buffer, but really this is the most
 // // basic test possible, and tests the test harness
@@ -76,115 +91,131 @@
 // }
 
 // DAWN_INSTANTIATE_TEST(BasicTests,
-//                       D3D12Backend(),
-//                       MetalBackend(),
-//                       OpenGLBackend(),
-//                       OpenGLESBackend(),
+//                       // D3D12Backend(),
+//                       // MetalBackend(),
+//                       // OpenGLBackend(),
+//                       // OpenGLESBackend(),
 //                       VulkanBackend());
 
 
 
 
+// ------------------------------------
+// end2end tests: 
 
 
 
 
-// ---------------------
-// unittests EnumClassBitmasksTests.cpp
 
-// Copyright 2017 The Dawn Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-#include "gtest/gtest.h"
 
-#include "dawn/EnumClassBitmasks.h"
 
-namespace dawn {
 
-    enum class Color : uint32_t {
-        R = 1,
-        G = 2,
-        B = 4,
-        A = 8,
-    };
 
-    template <>
-    struct IsDawnBitmask<Color> {
-        static constexpr bool enable = true;
-    };
 
-    TEST(BitmaskTests, BasicOperations) {
-        Color test1 = Color::R | Color::G;
-        ASSERT_EQ(1u | 2u, static_cast<uint32_t>(test1));
 
-        Color test2 = test1 ^ (Color::R | Color::A);
-        ASSERT_EQ(2u | 8u, static_cast<uint32_t>(test2));
 
-        Color test3 = test2 & Color::A;
-        ASSERT_EQ(8u, static_cast<uint32_t>(test3));
 
-        Color test4 = ~test3;
-        ASSERT_EQ(~uint32_t(8), static_cast<uint32_t>(test4));
-    }
 
-    TEST(BitmaskTests, AssignOperations) {
-        Color test1 = Color::R;
-        test1 |= Color::G;
-        ASSERT_EQ(1u | 2u, static_cast<uint32_t>(test1));
 
-        Color test2 = test1;
-        test2 ^= (Color::R | Color::A);
-        ASSERT_EQ(2u | 8u, static_cast<uint32_t>(test2));
 
-        Color test3 = test2;
-        test3 &= Color::A;
-        ASSERT_EQ(8u, static_cast<uint32_t>(test3));
-    }
 
-    TEST(BitmaskTests, BoolConversion) {
-        bool test1 = Color::R | Color::G;
-        ASSERT_TRUE(test1);
+// // ---------------------
+// // unittests EnumClassBitmasksTests.cpp
 
-        bool test2 = Color::R & Color::G;
-        ASSERT_FALSE(test2);
+// // Copyright 2017 The Dawn Authors
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
 
-        bool test3 = Color::R ^ Color::G;
-        ASSERT_TRUE(test3);
+// #include "gtest/gtest.h"
 
-        if (Color::R & ~Color::R) {
-            ASSERT_TRUE(false);
-        }
-    }
+// #include "dawn/EnumClassBitmasks.h"
 
-    TEST(BitmaskTests, ThreeOrs) {
-        Color c = Color::R | Color::G | Color::B;
-        ASSERT_EQ(7u, static_cast<uint32_t>(c));
-    }
+// namespace dawn {
 
-    TEST(BitmaskTests, ZeroOrOneBits) {
-        Color zero = static_cast<Color>(0);
-        ASSERT_TRUE(HasZeroOrOneBits(zero));
-        ASSERT_TRUE(HasZeroOrOneBits(Color::R));
-        ASSERT_TRUE(HasZeroOrOneBits(Color::G));
-        ASSERT_TRUE(HasZeroOrOneBits(Color::B));
-        ASSERT_TRUE(HasZeroOrOneBits(Color::A));
-        ASSERT_FALSE(HasZeroOrOneBits(static_cast<Color>(Color::R | Color::G)));
-        ASSERT_FALSE(HasZeroOrOneBits(static_cast<Color>(Color::G | Color::B)));
-        ASSERT_FALSE(HasZeroOrOneBits(static_cast<Color>(Color::B | Color::A)));
-    }
+//     enum class Color : uint32_t {
+//         R = 1,
+//         G = 2,
+//         B = 4,
+//         A = 8,
+//     };
 
-}  // namespace dawn
+//     template <>
+//     struct IsDawnBitmask<Color> {
+//         static constexpr bool enable = true;
+//     };
+
+//     TEST(BitmaskTests, BasicOperations) {
+//         Color test1 = Color::R | Color::G;
+//         ASSERT_EQ(1u | 2u, static_cast<uint32_t>(test1));
+
+//         Color test2 = test1 ^ (Color::R | Color::A);
+//         ASSERT_EQ(2u | 8u, static_cast<uint32_t>(test2));
+
+//         Color test3 = test2 & Color::A;
+//         ASSERT_EQ(8u, static_cast<uint32_t>(test3));
+
+//         Color test4 = ~test3;
+//         ASSERT_EQ(~uint32_t(8), static_cast<uint32_t>(test4));
+//     }
+
+//     TEST(BitmaskTests, AssignOperations) {
+//         Color test1 = Color::R;
+//         test1 |= Color::G;
+//         ASSERT_EQ(1u | 2u, static_cast<uint32_t>(test1));
+
+//         Color test2 = test1;
+//         test2 ^= (Color::R | Color::A);
+//         ASSERT_EQ(2u | 8u, static_cast<uint32_t>(test2));
+
+//         Color test3 = test2;
+//         test3 &= Color::A;
+//         ASSERT_EQ(8u, static_cast<uint32_t>(test3));
+//     }
+
+//     TEST(BitmaskTests, BoolConversion) {
+//         bool test1 = Color::R | Color::G;
+//         ASSERT_TRUE(test1);
+
+//         bool test2 = Color::R & Color::G;
+//         ASSERT_FALSE(test2);
+
+//         bool test3 = Color::R ^ Color::G;
+//         ASSERT_TRUE(test3);
+
+//         if (Color::R & ~Color::R) {
+//             ASSERT_TRUE(false);
+//         }
+//     }
+
+//     TEST(BitmaskTests, ThreeOrs) {
+//         Color c = Color::R | Color::G | Color::B;
+//         ASSERT_EQ(7u, static_cast<uint32_t>(c));
+//     }
+
+//     TEST(BitmaskTests, ZeroOrOneBits) {
+//         Color zero = static_cast<Color>(0);
+//         ASSERT_TRUE(HasZeroOrOneBits(zero));
+//         ASSERT_TRUE(HasZeroOrOneBits(Color::R));
+//         ASSERT_TRUE(HasZeroOrOneBits(Color::G));
+//         ASSERT_TRUE(HasZeroOrOneBits(Color::B));
+//         ASSERT_TRUE(HasZeroOrOneBits(Color::A));
+//         ASSERT_FALSE(HasZeroOrOneBits(static_cast<Color>(Color::R | Color::G)));
+//         ASSERT_FALSE(HasZeroOrOneBits(static_cast<Color>(Color::G | Color::B)));
+//         ASSERT_FALSE(HasZeroOrOneBits(static_cast<Color>(Color::B | Color::A)));
+//     }
+
+// }  // namespace dawn
 
 
 
@@ -210,7 +241,7 @@ namespace dawn {
 
 // #include "tests/unittests/validation/ValidationTest.h"
 
-// #include "utils/ComboRenderPipelineDescriptor.h"
+// // #include "utils/ComboRenderPipelineDescriptor.h"
 // #include "utils/WGPUHelpers.h"
 
 // namespace {
@@ -299,3 +330,304 @@ namespace dawn {
 
 // }  // namespace
 
+
+
+
+
+#include "common/Constants.h"
+#include "tests/unittests/validation/ValidationTest.h"
+#include "utils/WGPUHelpers.h"
+
+// TODO(cwallez@chromium.org): Add a regression test for Disptach validation trying to acces the
+// input state.
+
+class ComputeValidationTest : public ValidationTest {
+  protected:
+    void SetUp() override {
+        ValidationTest::SetUp();
+
+        wgpu::ShaderModule computeModule = utils::CreateShaderModule(device, R"(
+            [[stage(compute), workgroup_size(1)]] fn main() {
+            })");
+
+        // Set up compute pipeline
+        wgpu::PipelineLayout pl = utils::MakeBasicPipelineLayout(device, nullptr);
+
+        wgpu::ComputePipelineDescriptor csDesc;
+        csDesc.layout = pl;
+        csDesc.compute.module = computeModule;
+        csDesc.compute.entryPoint = "main";
+        pipeline = device.CreateComputePipeline(&csDesc);
+    }
+
+    void TestDispatch(uint32_t x, uint32_t y, uint32_t z) {
+        wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
+        wgpu::ComputePassEncoder pass = encoder.BeginComputePass();
+        pass.SetPipeline(pipeline);
+        pass.Dispatch(x, y, z);
+        pass.EndPass();
+        encoder.Finish();
+    }
+
+    wgpu::ComputePipeline pipeline;
+};
+
+// Check that 1x1x1 dispatch is OK.
+TEST_F(ComputeValidationTest, PerDimensionDispatchSizeLimits_SmallestValid) {
+    TestDispatch(1, 1, 1);
+}
+
+// // Check that the largest allowed dispatch is OK.
+// TEST_F(ComputeValidationTest, PerDimensionDispatchSizeLimits_LargestValid) {
+//     const uint32_t max = GetSupportedLimits().limits.maxComputeWorkgroupsPerDimension;
+//     TestDispatch(max, max, max);
+// }
+
+// // Check that exceeding the maximum on the X dimension results in validation failure.
+// TEST_F(ComputeValidationTest, PerDimensionDispatchSizeLimits_InvalidX) {
+//     const uint32_t max = GetSupportedLimits().limits.maxComputeWorkgroupsPerDimension;
+//     ASSERT_DEVICE_ERROR(TestDispatch(max + 1, 1, 1));
+// }
+
+// // Check that exceeding the maximum on the Y dimension results in validation failure.
+// TEST_F(ComputeValidationTest, PerDimensionDispatchSizeLimits_InvalidY) {
+//     const uint32_t max = GetSupportedLimits().limits.maxComputeWorkgroupsPerDimension;
+//     ASSERT_DEVICE_ERROR(TestDispatch(1, max + 1, 1));
+// }
+
+// // Check that exceeding the maximum on the Z dimension results in validation failure.
+// TEST_F(ComputeValidationTest, PerDimensionDispatchSizeLimits_InvalidZ) {
+//     const uint32_t max = GetSupportedLimits().limits.maxComputeWorkgroupsPerDimension;
+//     ASSERT_DEVICE_ERROR(TestDispatch(1, 1, max + 1));
+// }
+
+// // Check that exceeding the maximum on all dimensions results in validation failure.
+// TEST_F(ComputeValidationTest, PerDimensionDispatchSizeLimits_InvalidAll) {
+//     const uint32_t max = GetSupportedLimits().limits.maxComputeWorkgroupsPerDimension;
+//     ASSERT_DEVICE_ERROR(TestDispatch(max + 1, max + 1, max + 1));
+// }
+
+
+
+
+
+
+
+// #include "common/Constants.h"
+// #include "tests/unittests/validation/ValidationTest.h"
+// #include "utils/WGPUHelpers.h"
+
+// class ComputePipelineOverridableConstantsValidationTest : public ValidationTest {
+//   protected:
+//     void SetUpShadersWithDefaultValueConstants() {
+//         computeModule = utils::CreateShaderModule(device, R"(
+// [[override]] let c0: bool = true;      // type: bool
+// [[override]] let c1: bool = false;      // default override
+// [[override]] let c2: f32 = 0.0;         // type: float32
+// [[override]] let c3: f32 = 0.0;         // default override
+// [[override]] let c4: f32 = 4.0;         // default
+// [[override]] let c5: i32 = 0;           // type: int32
+// [[override]] let c6: i32 = 0;           // default override
+// [[override]] let c7: i32 = 7;           // default
+// [[override]] let c8: u32 = 0u;          // type: uint32
+// [[override]] let c9: u32 = 0u;          // default override
+// [[override(1000)]] let c10: u32 = 10u;  // default
+
+// [[stage(compute), workgroup_size(1)]] fn main() {
+//     // make sure the overridable constants are not optimized out
+//     _ = u32(c0);
+//     _ = u32(c1);
+//     _ = u32(c2);
+//     _ = u32(c3);
+//     _ = u32(c4);
+//     _ = u32(c5);
+//     _ = u32(c6);
+//     _ = u32(c7);
+//     _ = u32(c8);
+//     _ = u32(c9);
+//     _ = u32(c10);
+// })");
+//     }
+
+//     void SetUpShadersWithUninitializedConstants() {
+//         computeModule = utils::CreateShaderModule(device, R"(
+// [[override]] let c0: bool;              // type: bool
+// [[override]] let c1: bool = false;      // default override
+// [[override]] let c2: f32;               // type: float32
+// [[override]] let c3: f32 = 0.0;         // default override
+// [[override]] let c4: f32 = 4.0;         // default
+// [[override]] let c5: i32;               // type: int32
+// [[override]] let c6: i32 = 0;           // default override
+// [[override]] let c7: i32 = 7;           // default
+// [[override]] let c8: u32;               // type: uint32
+// [[override]] let c9: u32 = 0u;          // default override
+// [[override(1000)]] let c10: u32 = 10u;  // default
+
+// [[stage(compute), workgroup_size(1)]] fn main() {
+//     // make sure the overridable constants are not optimized out
+//     _ = u32(c0);
+//     _ = u32(c1);
+//     _ = u32(c2);
+//     _ = u32(c3);
+//     _ = u32(c4);
+//     _ = u32(c5);
+//     _ = u32(c6);
+//     _ = u32(c7);
+//     _ = u32(c8);
+//     _ = u32(c9);
+//     _ = u32(c10);
+// })");
+//     }
+
+//     void TestCreatePipeline(const std::vector<wgpu::ConstantEntry>& constants) {
+//         wgpu::ComputePipelineDescriptor csDesc;
+//         csDesc.compute.module = computeModule;
+//         csDesc.compute.entryPoint = "main";
+//         csDesc.compute.constants = constants.data();
+//         csDesc.compute.constantCount = constants.size();
+//         wgpu::ComputePipeline pipeline = device.CreateComputePipeline(&csDesc);
+//     }
+
+//     wgpu::ShaderModule computeModule;
+//     wgpu::Buffer buffer;
+// };
+
+// // Basic constants lookup tests
+// TEST_F(ComputePipelineOverridableConstantsValidationTest, ConstantsIdentifierLookUp) {
+//     SetUpShadersWithDefaultValueConstants();
+//     {
+//         // Valid: no constants specified
+//         std::vector<wgpu::ConstantEntry> constants;
+//         TestCreatePipeline(constants);
+//     }
+//     {
+//         // Valid: find by constant name
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "c0", 0}};
+//         TestCreatePipeline(constants);
+//     }
+//     {
+//         // Error: set the same constant twice
+//         std::vector<wgpu::ConstantEntry> constants{
+//             {nullptr, "c0", 0},
+//             {nullptr, "c0", 1},
+//         };
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+//     {
+//         // Valid: find by constant numeric id
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "1000", 0}};
+//         TestCreatePipeline(constants);
+//     }
+//     {
+//         // Error: constant numeric id not specified
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "9999", 0}};
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+//     {
+//         // Error: constant name doesn't exit
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "c99", 0}};
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+// }
+
+// // Test that it is invalid to leave any constants uninitialized
+// TEST_F(ComputePipelineOverridableConstantsValidationTest, UninitializedConstants) {
+//     SetUpShadersWithUninitializedConstants();
+//     {
+//         // Error: uninitialized constants exist
+//         std::vector<wgpu::ConstantEntry> constants;
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+//     {
+//         // Error: uninitialized constants exist
+//         std::vector<wgpu::ConstantEntry> constants{
+//             {nullptr, "c0", false},
+//             {nullptr, "c2", 1},
+//             // c5 is missing
+//             {nullptr, "c8", 1},
+//         };
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+//     {
+//         // Valid: all constants initialized
+//         std::vector<wgpu::ConstantEntry> constants{
+//             {nullptr, "c0", false},
+//             {nullptr, "c2", 1},
+//             {nullptr, "c5", 1},
+//             {nullptr, "c8", 1},
+//         };
+//         TestCreatePipeline(constants);
+//     }
+//     {
+//         // Error: duplicate initializations
+//         std::vector<wgpu::ConstantEntry> constants{
+//             {nullptr, "c0", false}, {nullptr, "c2", 1}, {nullptr, "c5", 1},
+//             {nullptr, "c8", 1},     {nullptr, "c2", 2},
+//         };
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+// }
+
+// // Test that only explicitly specified numeric ID can be referenced
+// TEST_F(ComputePipelineOverridableConstantsValidationTest, ConstantsIdentifierExplicitNumericID) {
+//     SetUpShadersWithDefaultValueConstants();
+//     {
+//         // Error: constant numeric id not explicitly specified
+//         // But could be impliciltly assigned to one of the constants
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "0", 0}};
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+//     {
+//         // Error: constant numeric id not explicitly specified
+//         // But could be impliciltly assigned to one of the constants
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "1", 0}};
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+//     {
+//         // Error: constant numeric id not explicitly specified
+//         // But could be impliciltly assigned to one of the constants
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "2", 0}};
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+//     {
+//         // Error: constant numeric id not explicitly specified
+//         // But could be impliciltly assigned to one of the constants
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "3", 0}};
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+// }
+
+// // Test that identifiers are unique
+// TEST_F(ComputePipelineOverridableConstantsValidationTest, ConstantsIdentifierUnique) {
+//     SetUpShadersWithDefaultValueConstants();
+//     {
+//         // Valid: constant without numeric id can be referenced with variable name
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "c0", 0}};
+//         TestCreatePipeline(constants);
+//     }
+//     {
+//         // Error: constant with numeric id cannot be referenced with variable name
+//         std::vector<wgpu::ConstantEntry> constants{{nullptr, "c10", 0}};
+//         ASSERT_DEVICE_ERROR(TestCreatePipeline(constants));
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// // #include "unittests/validation/ValidationTest.h"
+// #include "tests/unittests/validation/ValidationTest.h"
+
+// #include <gtest/gtest.h>
+
+// int main(int argc, char** argv) {
+//     InitDawnValidationTestEnvironment(argc, argv);
+//     testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
