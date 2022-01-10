688c0b94bed142afffed48c54f45ccc259befa26
Shrek Shao <shrekshao@google.com>
2022-01-10 14:14:27 -0800

    cmake changes

--- gtest_test.cpp
+++ gtest_test.cpp
@@ -13,6 +13,14 @@
 #include <cstdlib>
 #include <memory>
 
+// #define VALIDATION_TEST
+
+#ifndef VALIDATION_TEST
+#define END2END_TEST
+#else
+#undef END2END_TEST
+#endif
+
 // #include <gtest/gtest.h>
 // // #include "out/web/_deps/googletest-src/googletest/include/gtest/gtest.h"  // temp
 
@@ -26,24 +34,9 @@
 // }
 
 
-
+#ifdef END2END_TEST
 // // -------------------------------
 // // end2end BasicTests
-
-// // Copyright 2017 The Dawn Authors
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     http://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
-
 // #include "tests/DawnTest.h"
 
 // #include "utils/WGPUHelpers.h"
@@ -70,6 +63,7 @@
 //     wgpu::BufferDescriptor descriptor;
 //     descriptor.size = 4;
 //     descriptor.usage = wgpu::BufferUsage::CopySrc | wgpu::BufferUsage::CopyDst;
+//     // descriptor.usage = wgpu::BufferUsage::MapRead | wgpu::BufferUsage::CopyDst;
 //     wgpu::Buffer buffer = device.CreateBuffer(&descriptor);
 
 //     uint32_t value = 0x01020304;
@@ -78,30 +72,150 @@
 //     EXPECT_BUFFER_U32_EQ(value, buffer, 0);
 // }
 
-// // Test a validation error for Queue::WriteBuffer but really this is the most basic test possible
-// // for ASSERT_DEVICE_ERROR
-// TEST_P(BasicTests, QueueWriteBufferError) {
-//     wgpu::BufferDescriptor descriptor;
-//     descriptor.size = 4;
-//     descriptor.usage = wgpu::BufferUsage::CopySrc | wgpu::BufferUsage::CopyDst;
-//     wgpu::Buffer buffer = device.CreateBuffer(&descriptor);
+// // // Test a validation error for Queue::WriteBuffer but really this is the most basic test possible
+// // // for ASSERT_DEVICE_ERROR
+// // TEST_P(BasicTests, QueueWriteBufferError) {
+// //     wgpu::BufferDescriptor descriptor;
+// //     descriptor.size = 4;
+// //     descriptor.usage = wgpu::BufferUsage::CopySrc | wgpu::BufferUsage::CopyDst;
+// //     wgpu::Buffer buffer = device.CreateBuffer(&descriptor);
 
-//     uint8_t value = 187;
-//     ASSERT_DEVICE_ERROR(queue.WriteBuffer(buffer, 1000, &value, sizeof(value)));
-// }
+// //     uint8_t value = 187;
+// //     ASSERT_DEVICE_ERROR(queue.WriteBuffer(buffer, 1000, &value, sizeof(value)));
+// // }
 
 // DAWN_INSTANTIATE_TEST(BasicTests,
 //                       // D3D12Backend(),
 //                       // MetalBackend(),
 //                       // OpenGLBackend(),
 //                       // OpenGLESBackend(),
+//                       // WebGPUBackend(),
 //                       VulkanBackend());
 
 
 
 
 // ------------------------------------
-// end2end tests: 
+// end2end tests: BufferTests
+
+
+// #include "tests/DawnTest.h"
+
+// #include <array>
+// #include <cstring>
+
+// class BufferMappingTests : public DawnTest {
+//   protected:
+//     void MapAsyncAndWait(const wgpu::Buffer& buffer,
+//                          wgpu::MapMode mode,
+//                          size_t offset,
+//                          size_t size) {
+//         bool done = false;
+//         buffer.MapAsync(
+//             mode, offset, size,
+//             [](WGPUBufferMapAsyncStatus status, void* userdata) {
+//                 ASSERT_EQ(WGPUBufferMapAsyncStatus_Success, status);
+//                 *static_cast<bool*>(userdata) = true;
+//             },
+//             &done);
+
+//         while (!done) {
+//             WaitABit();
+//         }
+//     }
+
+//     wgpu::Buffer CreateMapReadBuffer(uint64_t size) {
+//         wgpu::BufferDescriptor descriptor;
+//         descriptor.size = size;
+//         descriptor.usage = wgpu::BufferUsage::MapRead | wgpu::BufferUsage::CopyDst;
+//         return device.CreateBuffer(&descriptor);
+//     }
+
+//     wgpu::Buffer CreateMapWriteBuffer(uint64_t size) {
+//         wgpu::BufferDescriptor descriptor;
+//         descriptor.size = size;
+//         descriptor.usage = wgpu::BufferUsage::MapWrite | wgpu::BufferUsage::CopySrc;
+//         return device.CreateBuffer(&descriptor);
+//     }
+// };
+
+// void CheckMapping(const void* actual, const void* expected, size_t size) {
+//     EXPECT_NE(actual, nullptr);
+//     if (actual != nullptr) {
+//         EXPECT_EQ(0, memcmp(actual, expected, size));
+//     }
+// }
+
+// // // Test that the simplest map read works
+// // TEST_P(BufferMappingTests, MapRead_Basic) {
+// //     wgpu::Buffer buffer = CreateMapReadBuffer(4);
+
+// //     uint32_t myData = 0x01020304;
+// //     constexpr size_t kSize = sizeof(myData);
+// //     queue.WriteBuffer(buffer, 0, &myData, kSize);
+
+// //     MapAsyncAndWait(buffer, wgpu::MapMode::Read, 0, 4);
+// //     CheckMapping(buffer.GetConstMappedRange(), &myData, kSize);
+// //     // CheckMapping(buffer.GetConstMappedRange(0, kSize), &myData, kSize);
+// //     buffer.Unmap();
+// // }
+
+// // // Test map-reading a zero-sized buffer.
+// // TEST_P(BufferMappingTests, MapRead_ZeroSized) {
+// //     wgpu::Buffer buffer = CreateMapReadBuffer(0);
+
+// //     MapAsyncAndWait(buffer, wgpu::MapMode::Read, 0, wgpu::kWholeMapSize);
+// //     ASSERT_NE(buffer.GetConstMappedRange(), nullptr);
+// //     buffer.Unmap();
+// // }
+
+// // // Test map-reading with a non-zero offset
+// // TEST_P(BufferMappingTests, MapRead_NonZeroOffset) {
+// //     wgpu::Buffer buffer = CreateMapReadBuffer(12);
+
+// //     uint32_t myData[3] = {0x01020304, 0x05060708, 0x090A0B0C};
+// //     queue.WriteBuffer(buffer, 0, &myData, sizeof(myData));
+
+// //     MapAsyncAndWait(buffer, wgpu::MapMode::Read, 8, 4);
+// //     ASSERT_EQ(myData[2], *static_cast<const uint32_t*>(buffer.GetConstMappedRange(8)));
+// //     buffer.Unmap();
+// // }
+
+// // Test that the simplest map write works.
+// TEST_P(BufferMappingTests, MapWrite_Basic) {
+//     wgpu::Buffer buffer = CreateMapWriteBuffer(4);
+
+//     uint32_t myData = 2934875;
+//     MapAsyncAndWait(buffer, wgpu::MapMode::Write, 0, 4);
+//     ASSERT_NE(nullptr, buffer.GetMappedRange());
+//     ASSERT_NE(nullptr, buffer.GetConstMappedRange());
+//     memcpy(buffer.GetMappedRange(), &myData, sizeof(myData));
+//     buffer.Unmap();
+
+//     EXPECT_BUFFER_U32_EQ(myData, buffer, 0);
+// }
+
+// // // Test that the simplest map write works with a range.
+// // TEST_P(BufferMappingTests, MapWrite_BasicRange) {
+// //     wgpu::Buffer buffer = CreateMapWriteBuffer(4);
+
+// //     uint32_t myData = 2934875;
+// //     MapAsyncAndWait(buffer, wgpu::MapMode::Write, 0, 4);
+// //     ASSERT_NE(nullptr, buffer.GetMappedRange(0, 4));
+// //     ASSERT_NE(nullptr, buffer.GetConstMappedRange(0, 4));
+// //     memcpy(buffer.GetMappedRange(), &myData, sizeof(myData));
+// //     buffer.Unmap();
+
+// //     EXPECT_BUFFER_U32_EQ(myData, buffer, 0);
+// // }
+
+// DAWN_INSTANTIATE_TEST(BufferMappingTests,
+//                       // D3D12Backend(),
+//                       // MetalBackend(),
+//                       // OpenGLBackend(),
+//                       // OpenGLESBackend(),
+//                       // WebGPUBackend(),
+//                       VulkanBackend());
 
 
 
@@ -114,9 +228,143 @@
 
 
 
+// //--------------------------------
+// // end2end tests: CullingTests.cpp
+
+// #include "tests/DawnTest.h"
+
+// #include "utils/ComboRenderPipelineDescriptor.h"
+// #include "utils/WGPUHelpers.h"
+
+// class CullingTest : public DawnTest {
+//   protected:
+//     wgpu::RenderPipeline CreatePipelineForTest(wgpu::FrontFace frontFace, wgpu::CullMode cullMode) {
+//         utils::ComboRenderPipelineDescriptor pipelineDescriptor;
+
+//         // Draw two triangles with different winding orders:
+//         // 1. The top-left one is counterclockwise (CCW)
+//         // 2. The bottom-right one is clockwise (CW)
+//         pipelineDescriptor.vertex.module = utils::CreateShaderModule(device, R"(
+//             [[stage(vertex)]]
+//             fn main([[builtin(vertex_index)]] VertexIndex : u32) -> [[builtin(position)]] vec4<f32> {
+//                 var pos = array<vec2<f32>, 6>(
+//                     vec2<f32>(-1.0,  1.0),
+//                     vec2<f32>(-1.0,  0.0),
+//                     vec2<f32>( 0.0,  1.0),
+//                     vec2<f32>( 0.0, -1.0),
+//                     vec2<f32>( 1.0,  0.0),
+//                     vec2<f32>( 1.0, -1.0));
+//                 return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+//             })");
+
+//         // FragCoord of pixel(x, y) in framebuffer coordinate is (x + 0.5, y + 0.5). And we use
+//         // RGBA8 format for the back buffer. So (FragCoord.xy - vec2(0.5)) / 255 in shader code
+//         // will make the pixel's R and G channels exactly equal to the pixel's x and y coordinates.
+//         pipelineDescriptor.cFragment.module = utils::CreateShaderModule(device, R"(
+//             [[stage(fragment)]]
+//             fn main([[builtin(position)]] FragCoord : vec4<f32>) -> [[location(0)]] vec4<f32> {
+//                 return vec4<f32>(
+//                     (FragCoord.xy - vec2<f32>(0.5, 0.5)) / vec2<f32>(255.0, 255.0),
+//                     0.0, 1.0);
+//             })");
+
+//         // Set culling mode and front face according to the parameters
+//         pipelineDescriptor.primitive.frontFace = frontFace;
+//         pipelineDescriptor.primitive.cullMode = cullMode;
+
+//         return device.CreateRenderPipeline(&pipelineDescriptor);
+//     }
+
+//     wgpu::Texture Create2DTextureForTest(wgpu::TextureFormat format) {
+//         wgpu::TextureDescriptor textureDescriptor;
+//         textureDescriptor.dimension = wgpu::TextureDimension::e2D;
+//         textureDescriptor.format = format;
+//         textureDescriptor.usage =
+//             wgpu::TextureUsage::RenderAttachment | wgpu::TextureUsage::CopySrc;
+//         textureDescriptor.mipLevelCount = 1;
+//         textureDescriptor.sampleCount = 1;
+//         textureDescriptor.size = {kSize, kSize, 1};
+//         return device.CreateTexture(&textureDescriptor);
+//     }
+
+//     void DoTest(wgpu::FrontFace frontFace,
+//                 wgpu::CullMode cullMode,
+//                 bool isCCWTriangleCulled,
+//                 bool isCWTriangleCulled) {
+//         wgpu::Texture colorTexture = Create2DTextureForTest(wgpu::TextureFormat::RGBA8Unorm);
+
+//         utils::ComboRenderPassDescriptor renderPassDescriptor({colorTexture.CreateView()});
+//         renderPassDescriptor.cColorAttachments[0].clearColor = {0.0, 0.0, 1.0, 1.0};
+//         renderPassDescriptor.cColorAttachments[0].loadOp = wgpu::LoadOp::Clear;
+
+//         wgpu::CommandEncoder commandEncoder = device.CreateCommandEncoder();
+//         wgpu::RenderPassEncoder renderPass = commandEncoder.BeginRenderPass(&renderPassDescriptor);
+//         renderPass.SetPipeline(CreatePipelineForTest(frontFace, cullMode));
+//         renderPass.Draw(6);
+//         renderPass.EndPass();
+//         wgpu::CommandBuffer commandBuffer = commandEncoder.Finish();
+//         queue.Submit(1, &commandBuffer);
+
+//         const RGBA8 kBackgroundColor = RGBA8::kBlue;
+//         const RGBA8 kTopLeftColor = RGBA8::kBlack;
+//         constexpr RGBA8 kBottomRightColor = RGBA8(3, 3, 0, 255);
+
+//         RGBA8 kCCWTriangleTopLeftColor = isCCWTriangleCulled ? kBackgroundColor : kTopLeftColor;
+//         EXPECT_PIXEL_RGBA8_EQ(kCCWTriangleTopLeftColor, colorTexture, 0, 0);
+
+//         RGBA8 kCWTriangleBottomRightColor =
+//             isCWTriangleCulled ? kBackgroundColor : kBottomRightColor;
+//         EXPECT_PIXEL_RGBA8_EQ(kCWTriangleBottomRightColor, colorTexture, kSize - 1, kSize - 1);
+//     }
+
+//     static constexpr uint32_t kSize = 4;
+// };
+
+// TEST_P(CullingTest, CullNoneWhenCCWIsFrontFace) {
+//     DoTest(wgpu::FrontFace::CCW, wgpu::CullMode::None, false, false);
+// }
+
+// // TEST_P(CullingTest, CullFrontFaceWhenCCWIsFrontFace) {
+// //     DoTest(wgpu::FrontFace::CCW, wgpu::CullMode::Front, true, false);
+// // }
+
+// // TEST_P(CullingTest, CullBackFaceWhenCCWIsFrontFace) {
+// //     DoTest(wgpu::FrontFace::CCW, wgpu::CullMode::Back, false, true);
+// // }
+
+// // TEST_P(CullingTest, CullNoneWhenCWIsFrontFace) {
+// //     DoTest(wgpu::FrontFace::CW, wgpu::CullMode::None, false, false);
+// // }
+
+// // TEST_P(CullingTest, CullFrontFaceWhenCWIsFrontFace) {
+// //     DoTest(wgpu::FrontFace::CW, wgpu::CullMode::Front, false, true);
+// // }
+
+// // TEST_P(CullingTest, CullBackFaceWhenCWIsFrontFace) {
+// //     DoTest(wgpu::FrontFace::CW, wgpu::CullMode::Back, true, false);
+// // }
+
+// DAWN_INSTANTIATE_TEST(CullingTest,
+//                     //   D3D12Backend(),
+//                     //   MetalBackend(),
+//                     //   OpenGLBackend(),
+//                     //   OpenGLESBackend(),
+//                       VulkanBackend());
 
 
 
+#endif
+
+
+
+
+
+
+
+
+
+
+#ifdef VALIDATION_TEST
 
 
 
@@ -613,7 +861,7 @@ TEST_F(ComputeValidationTest, PerDimensionDispatchSizeLimits_SmallestValid) {
 // }
 
 
-
+#endif
 
 
 
